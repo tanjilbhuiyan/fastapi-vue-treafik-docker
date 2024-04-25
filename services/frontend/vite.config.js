5d88a80fbb4e64414961129c7b4a24a9efe03009
tanjilbs23 <tanjil.bs23@gmail.com>
2024-04-25 14:52:51 +0600

    Update docker-compose.yml to include frontend service with Traefik and expose ports

--- services/frontend/vite.config.js
+++ services/frontend/vite.config.js
@@ -3,5 +3,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [vue()],
-})
+  preview: {
+   port: 5173,
+   strictPort: true,
+  },
+  server: {
+   port: 5173,
+   strictPort: true,
+   host: true,
+   origin: "http://0.0.0.0:5173",
+  },
+ });
